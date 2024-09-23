bbfef81e3894ed7e665b43325584e6b0a5fc4036
Heartscars <onlybuyforyou@gmail.com>
2024-09-23 18:34:04 +0800

    新增成績網站開場動畫

--- project4/app.js
+++ project4/app.js
@@ -8,9 +8,34 @@ const time_line = new TimelineMax();
 // 第二項是時間
 // 第三項是對像的初始狀態
 // 第四是對象的結束狀態
-time_line.fromTo(
-    hero,
-    1,
-    { height: "0%" },
-    { height: "100%", ease: Power2.easeInOut }
-);
+// 第五是動畫開始時間
+time_line
+    .fromTo(
+        hero,
+        1,
+        { height: "0%" },
+        { height: "100%", ease: Power2.easeInOut }
+    )
+    .fromTo(
+        hero,
+        1.2,
+        { width: "80%" },
+        { width: "100%", ease: Power2.easeInOut }
+    )
+    .fromTo(
+        slider,
+        1,
+        { x: "-100%" },
+        { x: "0%", ease: Power2.easeInOut },
+        "-=1.2"
+    )
+    .fromTo(
+        animation,
+        0.3,
+        { opacity: 1 },
+        { opacity: 0, ease: Power2.easeInOut }
+    );
+
+setTimeout(() => {
+    animation.style.pointerEvents = "none";
+}, 2500);
